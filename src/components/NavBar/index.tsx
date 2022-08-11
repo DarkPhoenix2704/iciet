d08271ca6fd7ad33e113589a5ce35c1c0a175f56
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-08-12 01:23:02 +0530

    Make navbar responsive

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,41 +1,58 @@
-import React from "react";
+import React, { MutableRefObject, ReactElement, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { Menu } from "react-feather";
 import "./index.css";
 const NavBar = () => {
+    const ref = useRef();
     return (
-        <nav>
-            <div className="navBarLinks">
-                <NavLink className="navBarItem" to="/">HOME</NavLink>
-                <div className="dropdown">
-                    <button className="navBarItem">ABOUT
-                    </button>
-                    <div className="dropdown-content">
-                        <NavLink className="navBarDropItem" to="/organizers">ORGANIZERS</NavLink>
-                        <NavLink className="navBarDropItem" to="/venue">VENUE</NavLink>
-                        <NavLink className="navBarDropItem" to="/accomodation">ACCOMODATION</NavLink>
-                    </div>    
+        <>
+            <nav>
+                <div className="navBarLinks">
+                    <NavLink className="navBarItem" to="/">HOME</NavLink>
+                    <div className="dropdown">
+                        <button className="navBarItem">ABOUT
+                        </button>
+                        <div className="dropdown-content">
+                            <NavLink className="navBarDropItem" to="/organizers">ORGANIZERS</NavLink>
+                            <NavLink className="navBarDropItem" to="/venue">VENUE</NavLink>
+                            <NavLink className="navBarDropItem" to="/accomodation">ACCOMODATION</NavLink>
+                        </div>    
+                    </div>
+                    <div className="dropdown">
+                        <button className="navBarItem">AUTHORS
+                        </button>
+                        <div className="dropdown-content">
+                            <NavLink className="navBarDropItem" to="/papers">CALL FOR PAPERS</NavLink>
+                            <NavLink className="navBarDropItem" to="/submission">SUBMISSION</NavLink>
+                            <NavLink className="navBarDropItem" to="/dates">IMPORTANT DATES</NavLink>
+                        </div>    
+                    </div>
+                    <NavLink className="navBarItem" to="/committe">COMMITTE</NavLink>
+                    <NavLink className="navBarItem" to="/registration">REGISTRATION</NavLink>
+                    <NavLink className="navBarItem" to="/contact">CONTACT</NavLink>
                 </div>
-                <div className="dropdown">
-                    <button className="navBarItem">AUTHORS
-                    </button>
-                    <div className="dropdown-content">
-                        <NavLink className="navBarDropItem" to="/papers">CALL FOR PAPERS</NavLink>
-                        <NavLink className="navBarDropItem" to="/submission">SUBMISSION</NavLink>
-                        <NavLink className="navBarDropItem" to="/dates">IMPORTANT DATES</NavLink>
-                    </div>    
+                <div className="menu" onClick={ () => {
+                    ref.current.style.display === "none" ? ref.current.style.display = "flex" : ref.current.style.display = "none"
+                }}>
+                    <p>
+                        MENU
+                    </p>
+                    <Menu color="#1E1E1E" size={36} className="menuIcon"/>
                 </div>
-                <NavLink className="navBarItem" to="/committe">COMMITTE</NavLink>
-                <NavLink className="navBarItem" to="/registration">REGISTRATION</NavLink>
-                <NavLink className="navBarItem" to="/contact">CONTACT</NavLink>
+            </nav>
+            <div className="mobileNav" ref={ref}>
+                <NavLink className="mobileNavItem" to="/">HOME</NavLink>
+                <NavLink className="mobileNavItem" to="/organizers">ORGANIZERS</NavLink>
+                <NavLink className="mobileNavItem" to="/venue">VENUE</NavLink>
+                <NavLink className="mobileNavItem" to="/accomodation">ACCOMODATION</NavLink>
+                <NavLink className="mobileNavItem" to="/papers">CALL FOR PAPERS</NavLink>
+                <NavLink className="mobileNavItem" to="/submission">SUBMISSION</NavLink>
+                <NavLink className="mobileNavItem" to="/dates">IMPORTANT DATES</NavLink>
+                <NavLink className="mobileNavItem" to="/committe">COMMITTE</NavLink>
+                <NavLink className="mobileNavItem" to="/registration">REGISTRATION</NavLink>
+                <NavLink className="mobileNavItem" to="/contact">CONTACT</NavLink>
             </div>
-            <div className="menu">
-                <p>
-                    MENU
-                </p>
-                <Menu color="#1E1E1E" size={36} className="menuIcon"/>
-            </div>
-        </nav>
+        </>
     );
 ;}
 export default NavBar;
